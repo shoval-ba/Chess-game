0a77f9ab3e00cee1f77f0c17555a419519673803
shoval2000 <shovalshoval31@gmail.com>
2022-04-27 17:26:39 +0300

    cant move if after he move the king is theart

--- boardData.js
+++ boardData.js
@@ -1,82 +1,67 @@
 class BoardData {
-    constructor(pieces) {
-      this.pieces = pieces;
-      this.turn = WHITE_PLAYER
-    }
-  
-    // Returns piece in row, col, or undefined if not exists.
-    getPiece(row, col) {
-      for (let piece of this.pieces) {
-        if (piece.row === row && piece.col === col) {
-          return piece;
-        }
-      }
-      return false;
-    }
-  
-    // Set location to the piece(on the click function) and delete the other piece if piece he eat him
-    setLocation(row, col, piece) {
-      if (this.turn === piece.player) {
-        const isOccupied = this.getPiece(row, col);
-        if (isOccupied) {
-          isOccupied.deletePiece();
-        }
-        piece.MoveLocation(row, col);
-        this.nextTurn();
-      }
-    }
-  
-    // To decide who`s turn
-    nextTurn() {
-      this.turn = this.turn == WHITE_PLAYER ? BLACK_PLAYER : WHITE_PLAYER
-    }
-  
-    // If the king is threat
-    isThreat() {
-      for (let piece of this.pieces) {
-        let possibleMoves = piece.possibleMoves();
-        for (let possibleMove of possibleMoves) {
-          if (this.getPiece(possibleMove[0], possibleMove[1]).type === KING) {
-            check.classList.remove("out");
-            check.classList.add("check");
-            isThreat = true;
-          }
-          // else
-          //   isThreat = false
-        }
-      }
-      check.classList.remove("check");
-      check.classList.add("out");
-      return isThreat
-    }
-  
-    // Check if i can move when the king is threat
-    checkIfCanMove(piece) {
-  
-      let possibleMoves = piece.piece.possibleMoves();
-      for (let possibleMove of possibleMoves) {
-        pieceNew = new state(piece, cell);
-  
-        pieceNew.player = piece.player
-        pieceNew.type = piece.type
-        pieceNew.row = possibleMove[0]
-        pieceNew.col = possibleMove[1]
-        pieceNew.cell = possibleMove
-  
-        this.setLocation(pieceNew.row, pieceNew.col, pieceOld)
-        console.log(pieceNew)
-  
-        this.isThreat()
-        if (isThreat === true) {
-          console.log("not move");
-          canMove = false;
-        }
-        else {
-          console.log("yes move");
-          canMove = true;
-        }
-  
-      }
-    }
-  
+  constructor(pieces) {
+    this.pieces = pieces;
+    this.turn = WHITE_PLAYER
   }
+
+  // Returns piece in row, col, or undefined if not exists.
+  getPiece(row, col) {
+    for (let piece of this.pieces) {
+      if (piece.row === row && piece.col === col) {
+        return piece;
+      }
+    }
+    return false;
+  }
+
+  // Set location to the piece(on the click function) and delete the other piece if piece he eat him
+  setLocation(row, col, piece) {
+    if (this.turn === piece.player) {
+      const isOccupied = this.getPiece(row, col);
+      if (isOccupied) {
+        isOccupied.deletePiece();
+      }
+      piece.MoveLocation(row, col);
+      this.nextTurn();
+    }
+  }
+
+  // To decide who`s turn
+  nextTurn() {
+    this.turn = this.turn == WHITE_PLAYER ? BLACK_PLAYER : WHITE_PLAYER
+  }
+
+  // If the king is threat
+  isThreat() {
+    for (let piece of this.pieces) {
+      let possibleMoves = piece.possibleMoves();
+      for (let possibleMove of possibleMoves) {
+        if (this.getPiece(possibleMove[0], possibleMove[1]).type === KING) {
+          check.classList.remove("out");
+          check.classList.add("check");
+          isThreat = true;
+        }
+        // else
+        //   isThreat = false
+      }
+    }
+    check.classList.remove("check");
+    check.classList.add("out");
+    return isThreat
+  }
+
+  // Check if i can move when the king is threat
+  checkIfCanMove(piece, possibleMoves = []) {
+let curPiece = piece.piece
+    let moves = possibleMoves.filter((move)=>{
+      let row = curPiece.row
+      let col = curPiece.col
+      curPiece.changeLocation(move.row, move.col)
+      let kingThreat = this.isThreat()
+      curPiece.changeLocation(row, col)
+      return !kingThreat
+    })
+    return moves
+  }
+
+}
